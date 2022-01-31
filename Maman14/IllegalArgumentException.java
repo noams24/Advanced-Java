2999ce4230d3b16fcda90b6eabf0a60cc96ac586
noams24 <80849999+noams24@users.noreply.github.com>
2022-01-31 21:36:40 +0200

    Add files via upload

---
+++ Maman14/IllegalArgumentException.java
@@ -0,0 +1,10 @@
+public class IllegalArgumentException extends Exception {
+
+    public IllegalArgumentException() {
+        super();
+    }
+
+    public IllegalArgumentException(String msg) {
+        super(msg);
+    }
+}
